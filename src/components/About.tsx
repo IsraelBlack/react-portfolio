b468eec7c0579a4297771e8d8da689440ba04deb
Yusuf <israelblack98@outlook.com>
2024-09-20 10:46:52 +1200

    shortened img urls

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ function About() {
 			className="flex flex-col md:flex-row items-center justify-center "
 			id="about"
 		>
-			<img src="/public/assets/programmer.svg" className="w-3/5 h-auto md:w-1/3" />
+			<img src="/assets/programmer.svg" className="w-3/5 h-auto md:w-1/3" />
 			<AboutItem />
 			<div id="skills"></div>
 		</div>
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,20 +1,20 @@
 export default [
 	{
-		imgUrl: '/public/assets/skills/react.svg',
+		imgUrl: '/assets/skills/react.svg',
 	},
 	{
-		imgUrl: '/public/assets/skills/Typescript.svg',
+		imgUrl: '/assets/skills/Typescript.svg',
 	},
 	{
-		imgUrl: '/public/assets/skills/tailwindcss.svg',
+		imgUrl: '/assets/skills/tailwindcss.svg',
 	},
 	{
-		imgUrl: '/public/assets/skills/sass.svg',
+		imgUrl: '/assets/skills/sass.svg',
 	},
 	{
-		imgUrl: '/public/assets/skills/redux.svg',
+		imgUrl: '/assets/skills/redux.svg',
 	},
 	{
-		imgUrl: '/public/assets/skills/git.svg',
+		imgUrl: '/assets/skills/git.svg',
 	},
 ];
